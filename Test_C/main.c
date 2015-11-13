70a90e1e0975e50ff36a7d2f430b20f5d6c00f28
Spectral-WMalquitar <welly.malquitar@spectralasia.net>
2015-11-13 08:35:55 +0800

    main.c Added for commit.

---
+++ Test_C/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Test_C
+//
+//  Created by Welly Malquitar on 11/11/2015.
+//  Copyright © 2015 Welly Malquitar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int x = 0;
+    
+    printf("Hello, please enter a number: ");
+    scanf("%d", &x);
+    
+    printf("The value entered is %d", x);
+    printf("Hello, World!\n");
+    return 0;
+}
